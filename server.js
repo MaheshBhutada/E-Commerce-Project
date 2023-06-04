7e2f6b7eb7e7be616cce93afe5bf98fecde3dbe5
MaheshBhutada <122001693+MaheshBhutada@users.noreply.github.com>
2023-06-04 16:07:03 +0530

    Update

--- server.js
+++ server.js
@@ -29,11 +29,18 @@ import cors from 'cors'
 
 import path from 'path'
 
+import { fileURLToPath } from 'url';
+
 // configure the file.
 config()
 
 // database config.
 connectDB();
+
+//esmodulefix.
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
